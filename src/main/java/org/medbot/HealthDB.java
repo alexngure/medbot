1bf1c3898cb635c3ff3fba3a3feba4a34f914f11
Alex Ngure <ngure001@umn.edu>
2016-09-20 14:51:48 -0500

    Consolidated api into a single source file

--- src/main/java/org/medbot/HealthDB.java
+++ src/main/java/org/medbot/HealthDB.java
@@ -1,6 +1,7 @@
 package org.medbot;
 
 import java.net.UnknownHostException;
+import java.util.HashMap;
 
 import javax.enterprise.context.RequestScoped;
 import javax.servlet.ServletException;
@@ -8,9 +9,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
-import java.util.Date;
-import java.util.HashMap;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.Mongo;
@@ -23,67 +21,218 @@ import com.mongodb.MongoException;
 import com.mongodb.client.MongoDatabase;
 
 @RequestScoped
-@Path("/db")
+@Path("/")
 public class HealthDB {
-//	private Mongo mongo;
-//	private DB mongoDB;
+	private MongoClient mongo;
+	private MongoDatabase mongoDB;
 
-//	public HealthDB() {
-//		String username = System.getenv("MONGODB_USER");
-//		String password = System.getenv("MONGODB_PASSWORD");
-//		String dbName = System.getenv("MONGODB_DATABASE");
-//		String host = "mongodb";//System.getenv("MONGODB_HOST");
-//		String port_string = "27017";//System.getenv("MONGODB_PORT");
-//		int port = Integer.decode(port_string);
-//
-//		try {
-//			mongo = new Mongo(host, port);
-//		} catch (UnknownHostException e) {
-//			//mongo = null;
-//			//System.out.println("unknown host exception");
-//		}
-//
-//		mongoDB = mongo.getDB(dbName);
-//
-//		if (mongoDB.authenticate(username, password.toCharArray()) == false) {
-//			//System.out.println("Couldn't authenticate MongoDB");
-//		} else {
-//			//System.out.println("Successfully accessed DB!");
-//		}
-//	}
-	
-	@GET()
-	@Produces("application/json")
-	public HashMap<String, String> getDB()
-	{
-	
+	public void openDBSession(){
 		String username = System.getenv("MONGODB_USER");
 		String password = System.getenv("MONGODB_PASSWORD");
 		String dbName = System.getenv("MONGODB_DATABASE");
-		String host = "mongodb";//System.getenv("MONGODB_HOST");
-		String port_string = "27017";//System.getenv("MONGODB_PORT");
-		int port = 27017;
-		
+
+		String uriString = "mongodb://" + username + ":" + password + "@mongodb/" + dbName;
+		mongo = new MongoClient(new MongoClientURI(uriString));
+		mongoDB = mongo.getDatabase(dbName);
+	}
+	
+
+	@GET()
+	@Produces("application/json")
+	public HashMap<String, String> getDB() {
 		HashMap<String, String> result = new HashMap<String, String>();
-		result.put("username", username);
-		result.put("password", password);
-		result.put("database", dbName);
-		
-		
-		String uriString = "mongodb://"+username+":"+password+"@mongodb/"+dbName;
-		MongoClient mongo = new MongoClient(new MongoClientURI(uriString));
-		MongoDatabase db = mongo.getDatabase(dbName);
-		String names = "1. ";
-		for(String name : db.listCollectionNames())
-		{
-			names += name;
-		}
-		
-		result.put("collections", names);
 		mongo.close();
 		return result;
 	}
 	
+	/**
+	 * Gets the given disease's information -- definition, symptoms, causes, side effects, etc.
+	 *
+	 * @param condition -- the disease name
+	 * @return a JSON tree with the info
+	 */
+	@GET()
+	@Path("/disease/{condition}")
+	@Produces("application/json")
+	public HashMap<String, String> getDisease(@PathParam("condition") String condition)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		return result;
+	}
 	
+	/**
+	 * Gets the disease's definition.
+	 *
+	 * @param condition -- the disease name
+	 * @return a JSON tree with the definition
+	 */
+	@GET()
+	@Path("/disease/definition/{condition}")
+	@Produces("application/json")
+	public HashMap<String, String> getDefinition(@PathParam("condition") String condition)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the disease's symptoms.
+	 *
+	 * @param condition -- the disease name
+	 * @return a JSON tree with the symptoms
+	 */
+	@GET()
+	@Path("/disease/symptoms/{condition}")
+	@Produces("application/json")
+	public HashMap<String, String> getSymptoms(@PathParam("condition") String condition)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the disease's cause.
+	 *
+	 * @param condition -- the disease name
+	 * @return a JSON tree with the cause info
+	 */
+	@GET()
+	@Path("/disease/cause/{condition}")
+	@Produces("application/json")
+	public HashMap<String, String> getCause(@PathParam("condition") String condition)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the drug info.
+	 *
+	 * @param drugName the drug name
+	 * @return a JSON tree with the drug info
+	 */
+	@GET()
+	@Path("/drug/{drugname}")
+	@Produces("application/json")
+	public HashMap<String, String> getDrugInfo(@PathParam("drugname") String drugName)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		return result;
+	}
+	
+	/**
+	 * Gets the drug definition.
+	 *
+	 * @param drugName the drug name
+	 * @return the drug definition (JSON tree)
+	 */
+	@GET()
+	@Path("/drug/definition/{drugname}")
+	@Produces("application/json")
+	public HashMap<String, String> getDrugDefinition(@PathParam("drugname") String drugName)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the drug side effects.
+	 *
+	 * @param drugName the drug name
+	 * @return the drug side effects (JSON tree)
+	 */
+	@GET()
+	@Path("/drug/effects/{drugname}")
+	@Produces("application/json")
+	public HashMap<String, String> getDrugSideEffects(@PathParam("drugname") String drugName)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the drug usage.
+	 *
+	 * @param drugName the drug name
+	 * @return the drug usage (JSON tree)
+	 */
+	@GET()
+	@Path("/drug/usage/{drugname}")
+	@Produces("application/json")
+	public HashMap<String, String> getDrugUsage(@PathParam("drugname") String drugName)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the drug precautions.
+	 *
+	 * @param drugName the drug name
+	 * @return the drug precautions (JSON tree)
+	 */
+	@GET()
+	@Path("/drug/precautions/{drugname}")
+	@Produces("application/json")
+	public HashMap<String, String> getDrugPrecautions(@PathParam("drugname") String drugName)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the drug storage.
+	 *
+	 * @param drugName the drug name
+	 * @return the drug storage instructions (JSON tree)
+	 */
+	@GET()
+	@Path("/drug/storage/{drugname}")
+	@Produces("application/json")
+	public HashMap<String, String> getDrugStorage(@PathParam("drugname") String drugName)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the drug's dietary restrictions.
+	 *
+	 * @param drugName the drug name
+	 * @return the drug's associated dietary info (JSON tree)
+	 */
+	@GET()
+	@Path("/drug/diet/{drugname}")
+	@Produces("application/json")
+	public HashMap<String, String> getDrugDiet(@PathParam("drugname") String drugName)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the drug overdose info.
+	 *
+	 * @param drugName the drug name
+	 * @return the drug overdose warnings
+	 */
+	@GET()
+	@Path("/drug/overdose/{drugname}")
+	@Produces("application/json")
+	public HashMap<String, String> getDrugOverdoseInfo(@PathParam("drugname") String drugName)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		return result;
+	}
 
 }
