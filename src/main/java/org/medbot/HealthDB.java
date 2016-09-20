b29bd7e866b082a685ac439c165da70af56fd131
Alex Ngure <ngure001@umn.edu>
2016-09-19 20:12:38 -0500

    added quick db test method

--- src/main/java/org/medbot/HealthDB.java
+++ src/main/java/org/medbot/HealthDB.java
@@ -1,8 +1,16 @@
 package org.medbot;
 
 import java.net.UnknownHostException;
+
+import javax.enterprise.context.RequestScoped;
 import javax.servlet.ServletException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 import java.util.Date;
+import java.util.HashMap;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.Mongo;
@@ -12,32 +20,53 @@ import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 
+@RequestScoped
+@Path("/db")
 public class HealthDB {
-	private Mongo mongo;
-	private DB mongoDB;
+//	private Mongo mongo;
+//	private DB mongoDB;
 
-	public HealthDB() {
+//	public HealthDB() {
+//		String username = System.getenv("MONGODB_USER");
+//		String password = System.getenv("MONGODB_PASSWORD");
+//		String dbName = System.getenv("MONGODB_DATABASE");
+//		String host = "mongodb";//System.getenv("MONGODB_HOST");
+//		String port_string = "27017";//System.getenv("MONGODB_PORT");
+//		int port = Integer.decode(port_string);
+//
+//		try {
+//			mongo = new Mongo(host, port);
+//		} catch (UnknownHostException e) {
+//			//mongo = null;
+//			//System.out.println("unknown host exception");
+//		}
+//
+//		mongoDB = mongo.getDB(dbName);
+//
+//		if (mongoDB.authenticate(username, password.toCharArray()) == false) {
+//			//System.out.println("Couldn't authenticate MongoDB");
+//		} else {
+//			//System.out.println("Successfully accessed DB!");
+//		}
+//	}
+	
+	@GET()
+	@Produces("application/json")
+	public HashMap<String, String> getDB()
+	{
 		String username = System.getenv("MONGODB_USER");
 		String password = System.getenv("MONGODB_PASSWORD");
 		String dbName = System.getenv("MONGODB_DATABASE");
 		String host = "mongodb";//System.getenv("MONGODB_HOST");
 		String port_string = "27017";//System.getenv("MONGODB_PORT");
-		int port = Integer.decode(port_string);
-
-		try {
-			mongo = new Mongo(host, port);
-		} catch (UnknownHostException e) {
-			//mongo = null;
-			//System.out.println("unknown host exception");
-		}
-
-		mongoDB = mongo.getDB(dbName);
-
-		if (mongoDB.authenticate(username, password.toCharArray()) == false) {
-			//System.out.println("Couldn't authenticate MongoDB");
-		} else {
-			//System.out.println("Successfully accessed DB!");
-		}
+		
+		HashMap<String, String> result = new HashMap<String, String>();
+		result.put("username", username);
+		result.put("password", password);
+		result.put("databse", dbName);
+		return result;
 	}
+	
+	
 
 }
